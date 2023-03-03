e91c2200dce243abc0d9d93a48ff791cd352c225
Lucas Bueno <53531008+LucasBusavs@users.noreply.github.com>
2023-03-02 22:45:42 -0300

    Create q4.c

---
+++ Lista 1/q4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int n;
+    printf("Insira um numero inteiro: ");
+    scanf("%d",&n);
+    printf("%d + 1 = %d",n,n+1);
+    return 0;
+}
