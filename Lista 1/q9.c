11ea4b1f164a19afcda1e3b0078c1524fbcfb4fa
Bueno Lucas <FD6241@engie.com>
2023-03-03 13:53:15 -0300

    Create q9.c

---
+++ Lista 1/q9.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    float valorI, taxa;
+    int tempo;
+
+    printf("Entre com o valor a ser investido: ");
+    scanf("%f", &valorI);
+    printf("Entre com o valor da taxa de investimento:");
+    scanf("%f", &taxa);
+    printf("Entre com o tempo em meses de aplicacao: ");
+    scanf("%f", &tempo);
+
+    
+    return 0;
+}
