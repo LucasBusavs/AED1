dc92249c662c4d6efb10ede711de6fedf351b92e
Lucas Bueno <53531008+LucasBusavs@users.noreply.github.com>
2023-03-04 13:39:40 -0300

    void jurosComposto

--- Lista 1/q9.c
+++ Lista 1/q9.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 void jurosSimples(float valorI, float taxa, int tempo, float* resSimples){
     float juros, mFinal;
@@ -8,6 +9,15 @@ void jurosSimples(float valorI, float taxa, int tempo, float* resSimples){
     mFinal = valorI+juros;
     resSimples[0] = juros;
     resSimples[1] = mFinal;
+}
+
+void jurosComposto(float valorI, float taxa, int tempo, float* resComposto){
+    float juros, mFinal;
+
+    mFinal = valorI*pow((1+taxa),tempo);
+    juros = mFinal - valorI;
+    resComposto[0] = juros;
+    resComposto[1] = mFinal;
 }
 
 int main(){
@@ -27,9 +37,14 @@ int main(){
         scanf("%d", &tempo);
 
     }
+
     jurosSimples(valorI, taxa, tempo, resSimples);
     printf("%.2f", resSimples[0]);
     printf("\n%.2f", resSimples[1]);
+
+    jurosComposto(valorI, taxa, tempo, resComposto);
+    printf("%.2f", resComposto[0]);
+    printf("\n%.2f", resComposto[1]);
 
     return 0;
 }
