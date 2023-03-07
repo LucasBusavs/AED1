3f7c1fdf921ab9e8048cafe639549e83b2210b8b
Lucas Bueno <53531008+LucasBusavs@users.noreply.github.com>
2023-03-06 21:01:55 -0300

    Create q2.c

---
+++ Lista 2/q2.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#define SENHA 1234
+
+int seqNumerica(char string[]){
+    int tamanho = strlen(string);
+
+    for(int i = 0; i<tamanho ; i++){
+        if(isdigit(string[i]) == 0){
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int main(){
+    char senha[5];
+    int isNumerico;
+
+    printf("Entre com uma senha de 4 algarismos: ");
+    scanf("%s", &senha);
+    
+    isNumerico = seqNumerica(senha);
+
+    while (strlen(senha)!=4 || isNumerico==0){
+        printf("A senha precisa ter 4 algarismos, entre novamente: ");
+        scanf("%s", &senha);
+        isNumerico = seqNumerica(senha);
+    }
+
+    return 0;
+}
