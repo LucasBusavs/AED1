675ca559c6ff964c22fbdafdb691fccf9dc80cdf
Lucas Bueno <53531008+LucasBusavs@users.noreply.github.com>
2023-04-03 19:18:49 -0300

    p1q2.c

---
+++ Correcao Prova 1/p1q2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int n;
+
+    printf("Entre com um valor inteiro positivo maior que 1: ");
+    scanf("%d",&n);
+    while (n<=1)
+    {
+        printf("Invalido. Entre com um valor inteiro positivo maior que 1: ");
+        scanf("%d",&n);
+    }
+
+    for(int i=2;i<=n;i++){  //Desconsidera o divisor 1, começando por i por 2
+        if(n%i==0){
+            printf("%d e um divisor de %d\n",i,n);
+        }
+    }
+    return 0;
+}
