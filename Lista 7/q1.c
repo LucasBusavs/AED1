972bfee62db5a1a1e6aab872ff50253bfed4e388
Lucas Bueno <turmadaciencia1@gmail.com>
2023-05-09 18:59:29 -0300

    q1.c Done

--- Lista 7/q1.c
+++ Lista 7/q1.c
@@ -2,11 +2,16 @@
 #include <string.h>
 #include <ctype.h>
 
+/*
+Descrição: Função contabiliza quantidade de alfanuméricos em uma string
+Parametros: vetor do tipo char(string)
+Return: Retorna variavel inteira com a quantidade de alfanuméricos
+*/
 int alphaNum(char *s){
     int qnt = 0;
     int size = strlen(s);
-    for(int i=0; i<=size; i++){
-        if(isalnum(s[i]) != 0){
+    for(int i=0; i<size; i++){
+        if(isalnum(s[i])){  //isalnum() retorna 0 se falso, >0 se verdadeiro
             qnt++;
         }
     }
@@ -18,12 +23,12 @@ int main(){
     int qnt;
 
     printf("Entre com uma string: ");
-    scanf("%[^\n]",&s);
+    scanf("%[^\n]",&s);     // Formato %[^\n] considera espaços
     fflush(stdin);
 
     qnt = alphaNum(s);
 
-    printf("%d",qnt);
+    printf("Quantidade de alfanumericos: %d\n",qnt);
 
     return 0;
 }
