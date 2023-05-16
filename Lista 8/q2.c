4659774648df528fdae8f35666fc5b3552e598d4
Bueno Lucas <FD6241@engie.com>
2023-05-16 11:11:19 -0300

    q2.c Done

--- Lista 8/q2.c
+++ Lista 8/q2.c
@@ -2,13 +2,18 @@
 #include <time.h>
 #include <stdlib.h>
 
+/*
+Descrição: Procedimento para calculo da soma da diagonal, e inicializar um vetor com os elementos da diagonal
+Parametros: int m, int a[m][m], int s[m]
+Retorno: "Retorna" por referencia o vetor preenchido
+*/
 void diagonal(int m, int a[m][m], int s[m]){
     int i, j;
     int soma = 0;
 
     for(i=0;i<m;i++){
         for(j=0;j<m;j++){
-            if(i == j){
+            if(i == j){ //Condição para diagonal principal
                 soma += a[i][j];
                 s[i] = a[i][j];
             }
@@ -17,14 +22,23 @@ void diagonal(int m, int a[m][m], int s[m]){
     printf("Soma da diagonal: %d\n", soma);
 }
 
+/*
+Descrição: Procedimento para inicialização de elementos inteiros de uma matriz m x m
+Parametros: int m, int a[m][m]
+Retorno: "Retorna" por referencia a matriz 'a' preenchida
+*/
 void inicializaMatriz(int m, int a[m][m]){
     for(int i=0;i<m;i++){
         for(int j=0;j<m;j++){
-            a[i][j] = rand() % 10;
+            a[i][j] = rand() % 10;  // 0 - 9
         }
     }
 }
 
+/*
+Descrição: Procedimento para printar uma matriz m x m
+Parametros: int m, int a[m][m]
+*/
 void mostraMatriz(int m, int a[m][m]){
     for(int i=0;i<m;i++){
         for(int j=0;j<m;j++){
@@ -34,6 +48,10 @@ void mostraMatriz(int m, int a[m][m]){
     }
 }
 
+/*
+Descrição: Procedimento para printar um vetor de tamanho m
+Parametros: int m, int s[m]
+*/
 void mostraVetor(int m, int s[m]){
     for(int i=0;i<m;i++){
         printf("%d ", s[i]);
@@ -47,6 +65,10 @@ int main(){
 
     printf("Entre com o tamanho m: ");
     scanf("%d", &m);
+    while(m < 1){
+        printf("Tamanho invalido. Entre novamente: ");
+        scanf("%d", &m);
+    }
 
     int a[m][m];
     int s[m];
