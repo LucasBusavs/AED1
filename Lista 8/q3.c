7b92b26886bf449bd61d63420e2d8733cea58d11
Bueno Lucas <FD6241@engie.com>
2023-05-16 12:09:52 -0300

    q3.c Done

--- Lista 8/q3.c
+++ Lista 8/q3.c
@@ -2,15 +2,24 @@
 #include <time.h>
 #include <stdlib.h>
 
+/*
+Descrição: Procedimento para inicialização de elementos inteiros de uma matriz n x m
+Parametros: int n, int m, int a[n][m]
+Retorno: "Retorna" por referencia a matriz r preenchida
+*/
 void inicializaMatriz(int n, int m, int a[n][m]){
     for(int i=0;i<n;i++){
         for(int j=0;j<m;j++){
-            a[i][j] = rand() % 10;
+            a[i][j] = rand() % 10; // 0 - 9
         }
     }
 }
 
-void mostraMatriz(int n, int m, int a[m][m]){
+/*
+Descrição: Procedimento para printar uma matriz n x m
+Parametros: int n, int m, int a[n][m]
+*/
+void mostraMatriz(int n, int m, int a[n][m]){
     for(int i=0;i<n;i++){
         for(int j=0;j<m;j++){
             printf("%d ",a[i][j]);
@@ -19,16 +28,20 @@ void mostraMatriz(int n, int m, int a[m][m]){
     }
 }
 
+/*
+Descrição: Procedimento para calcular e mostrar matriz c, resultante de a+b
+Parametros: int n, int m, int a[n][m], int b[n][m]
+*/
 void calculaC(int n, int m, int a[n][m], int b[n][m]){
     int c[n][m];
 
     for(int i=0;i<n;i++){
         for(int j=0;j<m;j++){
             c[i][j] = a[i][j] + b[i][j];
+            printf("%d ",c[i][j]);
         }
+        printf("\n");
     }
-
-    mostraMatriz(n, m, c);
 }
 
 int main(){
@@ -38,8 +51,17 @@ int main(){
 
     printf("Entre com o numero de linhas: ");
     scanf("%d", &n);
+    while (n < 1)
+    {
+        printf("Quantidade de linhas invalida. Entre novamente: ");
+        scanf("%d", &n);
+    }
     printf("Entre com o numero de colunas: ");
     scanf("%d",&m);
+    while(m < 1){
+        printf("Quantidade de colunas invalida. Entre novamente: ");
+        scanf("%d", &m);
+    }
 
     int a[n][m], b[n][m];
 
