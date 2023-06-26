7b2a43300548e09fb3089dc36d34ea59caef547c
Lucas Bueno <turmadaciencia1@gmail.com>
2023-06-26 18:36:01 -0300

    cadastroEntrada

--- Lista 10/1.cpp
+++ Lista 10/1.cpp
@@ -4,6 +4,8 @@ using namespace std;
 
 #define ANO_ATUAL 2023
 
+int globalI = 0;
+
 class Data {
     private:
         int dia;
@@ -126,36 +128,44 @@ class Cliente{
             string nome, cpf;
             int dia, mes, ano;
             int hora, min;
+
+            cout << "CADASTRO DE CLIENTE" << endl;
+
             cout << "Entre com o nome do cliente: ";
             cin >> nome;
             while(!setNome(nome)){
                 cout << "Nome invalido, Entre novamente: ";
                 cin >> nome;
             }
+
             cout << "Entre com o CPF do cliente: ";
             cin >> cpf;
             while(!setCPF(cpf)){
                 cout << "CPF invalido, Entre novamente: ";
                 cin >> cpf;
             }
+
             cout << "Entre com a data do cadastro(dia mes ano): ";
             cin >> dia >> mes >> ano;
             while(!cadastro.setData(dia, mes, ano)){
                 cout << "Data invalida. Entre novamente com a data do cadastro(dia mes ano): ";
                 cin >> dia >> mes >> ano;
             }
+
             cout << "Entre com o horario do cadastro(hora minuto): ";
             cin >> hora >> min;
             while(!cadastro.setHorario(hora, min)){
                 cout << "Horario invalido. Entre novamente com o horario do cadastro(hora minuto): ";
                 cin >> hora >> min;
             }
+
             cout << "CADASTRO REALIZADO COM SUCESSO" << endl;
         }
 
         void info(){
             cout << "Nome: " << nome << endl;
             cout << "CPF: " << cpf << endl;
+            cout << "Data e horario de cadastro" << endl;
             cadastro.mostraData();
             cadastro.mostraHorario();
         }
@@ -187,11 +197,99 @@ class Veiculos {
         float preco;
         Cliente cliente;
 
+    public:
+        Veiculos(){
+            cod = ++globalI;
+            placa = "";
+            modelo = "";
+            preco = 0;
+        }
+
+        int getCod() { return cod; }
+        string getPlaca() { return placa; }
+        bool setPlaca(string placa){
+            if(placa.length() == 7){
+                this->placa = placa;
+                return true;
+            }
+            return false;
+        }
+        string getModelo() { return modelo; }
+        bool setModelo(string modelo){
+            if(modelo.length() <= 30 && modelo.length() > 0){
+                this->modelo = modelo;
+                return true;
+            }
+            return false;
+        }
+        Data getEntrada() { return entrada; }
+        Data getSaida() { return saida; }
+        float getPreco() { return preco; }
+        Cliente getCliente() { return cliente; }
+
+        void cadastroEntrada(){
+            string placa, modelo;
+            int dia, mes, ano;
+            int hora, min;
+            char cliente;
+
+            cout << "CADASTRO DE ENTRADA" << endl;
+            
+            cout << "Entre com a placa: " << endl;
+            cin >> placa;
+            while(!setPlaca(placa)){
+                cout << "Placa Invalida. Entre com a placa: " << endl;
+                cin >> placa;
+            }
+            
+            cout << "Entre com o modelo: " << endl;
+            cin >> modelo;
+            while(!setModelo(modelo)){
+                cout << "Modelo Invalido. Entre com o modelo: " << endl;
+                cin >> modelo;
+            }
+            
+            cout << "Entre com a data da entrada(dia mes ano): ";
+            cin >> dia >> mes >> ano;
+            while(!entrada.setData(dia, mes, ano)){
+                cout << "Data invalida. Entre novamente com a data da entrada(dia mes ano): ";
+                cin >> dia >> mes >> ano;
+            }
+            
+            cout << "Entre com o horario da entrada(hora minuto): ";
+            cin >> hora >> min;
+            while(!entrada.setHorario(hora, min)){
+                cout << "Horario invalido. Entre novamente com o horario da entrada(hora minuto): ";
+                cin >> hora >> min;
+            }
+
+            cout << "O cliente vai querer cadastrar[y/N]: ";
+            cin >> cliente;
+            //Enter sem nada
+            if(cliente == ' '){
+                cliente = 'N';
+            }
+            cliente = toupper(cliente);
+            while(cliente != 'Y' && cliente != 'N'){
+                cout << "Invalido. O cliente vai querer cadastrar[y/N]: ";
+                cin >> cliente;
+                //Enter sem nada
+                if(cliente == ' '){
+                    cliente = 'N';
+                }
+            }
+
+            if(cliente == 'Y'){
+                this->cliente.cadastrar();
+            }
+
+        }
 };
 
 int main(){
     Data data;
     Cliente cliente;
+    Veiculos v;
     /*
     int dia, mes, ano;
     int hora, min;
@@ -208,7 +306,17 @@ int main(){
     data.mostraHorario();
     */
     //cliente.cadastrar();
-    cliente.info();
+    //cliente.info();
+    //cout << v.getCod() << endl;
+    v.cadastroEntrada();
+
+    cout << v.getPlaca() << endl;
+    cout << v.getModelo() << endl;
+    v.getEntrada().mostraData();
+    v.getEntrada().mostraHorario();
+    v.getCliente().info();
+
+
 
     return 0;
 }
